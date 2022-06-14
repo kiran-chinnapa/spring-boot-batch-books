0994686acd1400dd8c3e26adbcc8efe9e5c1af3a
kiran-chinnapa <kiranms.chinnapa@gmail.com>
2022-06-14 00:01:24 -0400

    Spring JPA implementation

--- src/main/java/com/batch/books/entity/Person.java
+++ src/main/java/com/batch/books/entity/Person.java
@@ -1,4 +1,6 @@
 package com.batch.books.entity;
+
+import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,70 +11,13 @@ import java.util.Date;
 @Entity
 @NamedQuery(name="find_all_persons", query="select p from Person p")
 @NamedQuery(name="find_person_using_like",query="select p from Person p where p.name like :param")
-public class Person {
+public @Data class Person {
 
     @Id
     @GeneratedValue
     private int id;
-
     private String name;
     private String location;
     private Date birthDate;
-
-    public Person() {
-
-    }
-
-    public Person(int id, String name, String location, Date birthDate) {
-        super();
-        this.id = id;
-        this.name = name;
-        this.location = location;
-        this.birthDate = birthDate;
-    }
-
-    public Person(String name, String location, Date birthDate) {
-        super();
-        this.name = name;
-        this.location = location;
-        this.birthDate = birthDate;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("\nPerson [id=%s, name=%s, location=%s, birthDate=%s]", id, name, location, birthDate);
-    }
 
 }
